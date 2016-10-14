406ba5499e790e351bfccd78eeb7b817c2522563
SakuraTheKitten <kryptokards@gmail.com>
2016-10-14 00:15:25 -0400

    initial upload

---
+++ routes/about.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('about', { title: 'Sakura Duff: About Me' });
+});
+
+module.exports = router;
